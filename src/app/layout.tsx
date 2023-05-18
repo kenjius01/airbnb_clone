c818b6a2b4105e1ac3644494c0087b03586cd608
kenjius01 <kenjius01@gmail.com>
2023-05-19 01:06:49 +0700

    config eslint and prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,11 @@ export const metadata = {
   description: 'AirBnB clone'
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang='en'>
       <body className={inter.className}>{children}</body>
